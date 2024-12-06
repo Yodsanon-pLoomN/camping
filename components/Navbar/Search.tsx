97c82220195fdccb4b8e90b9f7bf5dc0fe0d81d3
Yodsanon-pLoomN <pLoomN321654987@gmail.com>
2024-12-07 00:06:57 +0700

    day 1 commit

---
+++ components/Navbar/Search.tsx
@@ -0,0 +1,16 @@
+
+import { Input } from "@/components/ui/input"
+
+
+const Search = () => {
+  return (
+    <div>
+      <Input 
+      type="text"
+      placeholder="Search"
+      className="max-w-xs"
+      />
+   </div>
+  )
+}
+export default Search
